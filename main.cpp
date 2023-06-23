f9e50ec59d240fbb74c3ed70c58a8ee0e37fcce4
Eduardo Cordeiro <53461410+eduardoc7@users.noreply.github.com>
2023-06-23 11:09:02 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 #include <string>
+class classeLegal {
+public:
+  char *memberPublicPointerChar;
+  long double memberPublicLongDouble2;
+  long long memberPublicLongLong;
+  long memberPublicLong;
+  double memberPublicDouble2222222;
+  float memberPublicFloat = 0;
+  bool memberPublicBool = false;
+  int memberIntPublic{};
+};
 
 std::string hello(std::string name) {
   using namespace std::string_literals;
